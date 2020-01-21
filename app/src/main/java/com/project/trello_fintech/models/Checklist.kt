c40ab13c89935d6af265a2dbdf1ecdd54ac25b80
egsam98 <egsam98@gmail.com>
2020-01-21 07:59:29 +0300

    remove positions props in Checklist.kt and remove implementing IListItem

--- app/src/main/java/com/project/trello_fintech/models/Checklist.kt
+++ app/src/main/java/com/project/trello_fintech/models/Checklist.kt
@@ -5,26 +5,19 @@ import com.google.gson.annotations.SerializedName
 /**
  * Перечень действий для выполнения для одной конкретной задачи
  * @property title String
- * @property pos Int
  * @property items List<Item>
  */
 data class Checklist(
     @SerializedName("name") val title: String,
-    @SerializedName("pos") val pos: Int,
     @SerializedName("checkItems") val items: List<Item>
-): IListItem {
+) {
 
     data class Item(
         @SerializedName("name") val title: String,
-        @SerializedName("state") private val state: String,
-        @SerializedName("pos") val pos: Int
-    ): IListItem {
+        @SerializedName("state") private val state: String
+    ) {
 
         val isChecked: Boolean
             get() = state == "complete"
-
-        override fun getType(): Int = IListItem.BODY
     }
-
-    override fun getType(): Int = IListItem.HEADER
 }
